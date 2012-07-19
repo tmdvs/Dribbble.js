466f039240f540d485c925713f0e9b7248fd24b4
Tim Davies <timgrantdavies@me.com>
2012-07-19 20:33:00 +0100

    Publishing dribbble.js to github

---
+++ README.md
@@ -0,0 +1,17 @@
+#Dribbble.js
+Dribbble.js is a single file, library agnositc script for adding your recent Dribbble shots to your website. Dribbble.js is written in "old fashoined" JavaScript and doesn't need jQuery or other such libraries to operate, and can therefore be dropped into any site and just work. Below is an example of to implement Dribbble.js on your website
+
+```javascript
+<script src="scripts/dribbble.js" type="text/javascript"></script>
+<scripttype="text/javascript">
+    getShotsForID(2151, 'shots');
+</script>
+```
+
+The paramets for getShotsForID are as follows
+
+```javascript
+getShotsForID (dribble user id, element ID to add the shots to, number of shots)
+```
+
+For a more detailed example, check out the demo page source and the script file itself.
---
+++ scripts/dribbble.js
@@ -0,0 +1,50 @@
+/**
+* Dribbble.js
+*
+* @author Tim Davies
+*/
+
+/**
+* Fetch and parse Dribbble JSON
+*
+* @param dribbbleID   int      Dribbble User ID
+* @param elm          string   Dom Element ID to add the shots to (optional, defaults to 'shots')
+* @param limit        int      Number of shots to draw (optional, defaults to 3)
+*/
+
+function getShotsForID (dribbbleID, elm, limit)
+{	
+	/* Initialise funcation variables */
+	limit = (!limit)? 3 : limit;
+	elm = (!elm)? 'shots' : elm;
+	var apirequest = new XMLHttpRequest();  
+	var url = 'http://api.dribbble.com/players/'+dribbbleID.toString()+'/shots';  
+	var shots = [];
+	
+	/* Begin XMLHttpRequest/AJAX request */
+	apirequest.open('GET', url, true);  
+	apirequest.onreadystatechange = apiRequestHandler;  
+	apirequest.send();  
+	
+	/* Remote request handler */
+	function apiRequestHandler ()
+	{
+		/* When readyState becomes "DONE" draw shots */
+		if(apirequest.readyState == 4)
+		{
+			shots = JSON.parse(apirequest.response);
+			var htmlString = "\n<ul>\n"
+			
+			for (var i = 0; i < limit; i++)
+			{
+				var shot = shots.shots[i];
+				htmlString = htmlString+"\n<li class=\"dribbble_shot\">"
+				htmlString = htmlString+"<img src=\""+shot.image_url+"\" alt=\""+shot.title+"\" />"
+				htmlString = htmlString+"</li>\n";
+			}
+			
+			htmlString = htmlString + "\n</ul>\n";
+			document.getElementById(elm).innerHTML = htmlString;
+		}
+	}	
+}
