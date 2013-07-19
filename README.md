f9471f3121cd1ee9cbb93560816a765f84ba7a31
tmdvs <timgrantdavies@me.com>
2013-07-19 10:54:09 +0100

    Now supporting multiple users

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Dribbble.js is a single file, library agnositc script for adding your recent Dri
 ```javascript
 <script src="scripts/dribbble.js" type="text/javascript"></script>
 <script type="text/javascript">
-    getShotsForID('tim', 'shots');
+    getShotsForID(['tim', 'dribbble'], 'shots');
 </script>
 ```
 
 The paramets for getShotsForID are as follows
 
 ```javascript
-getShotsForID (dribble user id or username, element ID to add the shots to, number of shots)
+getShotsForID (array of dribble user id or username, element ID to add the shots to, number of shots)
 ```
 
 For a more detailed example, check out the demo page source and the script file itself.
--- scripts/dribbble.js
+++ scripts/dribbble.js
@@ -14,41 +14,66 @@
 
 // Define Global Vars
 var userID, shotLimit, element = '';
+var usersToLoad = 0;
+var all_shots = [];
 
 function getShotsForID (dribbbleID, elm, limit)
 {	
 	/* Initialise funcation variables */
 	shotLimit = (!limit)? 3 : limit;
 	element = (!elm)? 'shots' : elm;
-	userID = ''+dribbbleID;
+
+	usersToLoad = dribbbleID.length;
 	
 	document.addEventListener('DOMContentLoaded', function () {
-		var url = 'http://api.dribbble.com/players/'+userID+'/shots?callback=parseShots';  
-		var shots = [];
 		
-		/* Insert JSONP Script tag*/
-		var myscript = document.createElement('script');
-		myscript.src = url;
-		document.body.appendChild(myscript);
+		for(indx in dribbbleID)
+		{
+			var url = 'http://api.dribbble.com/players/'+dribbbleID[indx]+'/shots?callback=parseShots';  
+			var shots = [];
+	
+			/* Insert JSONP Script tag*/
+			var myscript = document.createElement('script');
+			myscript.src = url;
+			document.body.appendChild(myscript);
+		}
 	});
 }
+
 
 /* JSONP callback handler */
 function parseShots (shots)
 {	
-	var htmlString = "\n<ul>\n"
+	usersToLoad--;
+	all_shots.push.apply(all_shots, shots.shots);
 	
-	for (var i = 0; i < shotLimit; i++)
-	{
-		var shot = shots.shots[i];
-		htmlString = htmlString+"\n<li class=\"dribbble_shot\">";
-		htmlString = htmlString+"<a href=\""+shot.url+"\">";
-		htmlString = htmlString+"<img src=\""+shot.image_url+"\" alt=\""+shot.title+"\" />";
-		htmlString = htmlString+"</a>";
-		htmlString = htmlString+"</li>\n";
+	// Our basic compare to order shots
+	function compare(a,b) {
+	  if (a.id > b.id)
+		 return -1;
+	  if (a.id < b.id)
+		return 1;
+	  return 0;
 	}
-	
-	htmlString = htmlString + "\n</ul>\n";
-				
-	document.getElementById(element).innerHTML = htmlString;
+
+	if(usersToLoad == 0)
+	{
+		all_shots.sort(compare)
+		
+		var htmlString = "\n<ul>\n"
+
+		for (var i = 0; i < shotLimit; i++)
+		{
+			var shot = all_shots[i];
+			htmlString = htmlString+"\n<li class=\"dribbble_shot\">";
+			htmlString = htmlString+"<a href=\""+shot.url+"\">";
+			htmlString = htmlString+"<img src=\""+shot.image_url+"\" alt=\""+shot.title+"\" />";
+			htmlString = htmlString+"</a>";
+			htmlString = htmlString+"</li>\n";
+		}
+
+		htmlString = htmlString + "\n</ul>\n";
+
+		document.getElementById(element).innerHTML = htmlString;
+	}
 }
